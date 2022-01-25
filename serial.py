bbc836adfae6aa35ca39a8b1d405d31b9a680794
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-25 17:25:50 -0600

    finished first assignment

--- serial.py
+++ serial.py
@@ -17,3 +17,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    #CR What is the meaning of self.serial, clarify next coming out? or current serial?
+    
+    def __init__(self, start=100):
+        """ Create serial generator with start"""
+        self.start = start
+        self.next = start
+
+    def generate(self):
+        """ Increments self.serial by one and returns """
+        self.next += 1
+        return self.next - 1
+
+    def reset(self):
+        """ Resets to starting argument """
+        self.next = self.start - 1
