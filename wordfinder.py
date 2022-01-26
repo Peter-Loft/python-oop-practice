7f1a1961c63971d7ef702b5406bdeef9dd6f403c
Peter Loft <peterloft@Peters-MacBook-Pro.local>
2022-01-25 18:53:20 -0600

    CR notes

--- wordfinder.py
+++ wordfinder.py
@@ -1,10 +1,14 @@
+import random
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
     def __init__(self, path):
         self.path = path
-        self.wordList = self.read_file()
+        self.word_list = self.read_file()
     
-
+    # CR maybe think about opening the file in __init__
+    # so read_file becomes more parse or parse_contents and
+    # can handle polymorphism better with SpecialWordFinder
     def read_file(self):
         """ Get words from file line by line and return a list of those words """
 
@@ -15,9 +19,30 @@ class WordFinder:
 
         return word_list
 
-    def random():
-        """ Get random word from self.wordList """
+    def random(self):
+        """ Get random word from self.word_list """
+    #CR also think about random.choice(list) 
+        return self.word_list[random.randrange(0,len(self.word_list))]
 
 
+class SpecialWordFinder(WordFinder):
+    """ Special Word Finder inherits from WordFinder,
+     can parse blank lines and ignore comment lines
+     """
+    def __init__(self, path):
+        """ Initializing function, inherits with super(),
+        creates SpecialWordFinder which ignores comments
+        and new lines.
+         """
+        super().__init__(path)
+        self.word_list = self.handle_special_lines()
 
-        return
+    def handle_special_lines(self):
+        """ Removes comments and new lines from initally read
+        text file  """
+    # CR no need to set variable, just return it
+        return [wrd for wrd in self.word_list 
+            if not wrd == "" and 
+            not wrd.startswith("#")]
+                
+        
